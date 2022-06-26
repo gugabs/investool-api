6e65de3d6d3e94fac0836b55251dce38e9d374a7
Guga <contato.gustavobarbosa@outlook.com>
2022-06-26 00:05:35 -0300

    refactor: send html e-mail instead plain text

--- src/utils/emails.ts
+++ src/utils/emails.ts
@@ -4,8 +4,19 @@ export async function sendActivationCode(to: string, code: string) {
   const email = {
     to,
     from: process.env.SENDGRID_SENDER!,
-    subject: "[Investool] Activate your account",
-    text: code,
+    subject: "[Investool] Seu código de ativação de conta chegou!",
+    html: `
+      <h1>Ativação de Conta</h1>
+      <p>Olá, como vai você?</p>
+      <p>Para realizar a ativação da sua conta basta seguir os seguintes passos:</p>
+      <ol>
+        <li>Clicar no link fornecido neste e-mail;</li>
+        <li>Aguardar o redirecionamento para a plataforma;</li>
+        <li>Clicar no botão de ativação presente na página para a qual você foi redirecionado(a).</li>
+      </ol>
+      <a href="http://localhost:3000/activate-account/${code}">Ativar conta</a>
+      <hr>
+    `,
   };
 
   await sgMail.send(email);
